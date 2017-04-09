94be9a72ca7c7a4e79d55f37b3b3a762c799ca8f
wan <nishikawa0228@sj9.so-net.ne.jp>
2017-04-09 16:21:27 +0900

    refs #11 Upload rapidfire to pypi

--- setup.py
+++ setup.py
@@ -1,26 +1,47 @@
 # -*- coding:utf-8 -*-
-import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
+from rapidfire import (
+    __version__,
+    __license__,
+    __author__,
+    __author_email__,
+)
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+__name__ = 'rapidfire'
+__url__ = 'https://github.com/wanshot/RapidFire'
+
+__short_description__ = __name__ + ' calls and displays the defined Python function from command line'
+__long_description__ = open('./README.rst', 'r').read()
+
+__classifiers__ = [
+    'Programming Language :: Python',
+    'Development Status :: 5 - Production/Stable',
+    'Topic :: Software Development',
+    'Programming Language :: Python :: 3',
+    'Topic :: Terminals',
+]
+
+__keywords__ = [
+    'cli',
+    'command line interface',
+    'shell',
+]
 
 setup(
-    name='rapidfire',
-    version='0.0.0',
-    author='wanshot',
-    author_email='',
-    description='',
-    license='MIT',
-    keywords='',
-    long_description=read('README.rst'),
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-    ],
-    packages=['rapidfire'],
+    name=__name__,
+    version=__version__,
+    description=__short_description__,
+    long_description=__long_description__,
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    classifiers=__classifiers__,
+    keywords=' ,'.join(__keywords__),
+    license=__license__,
+    packages=find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': ['rap = rapidfire.run:main']
     },
-    install_requires=['argparse', 'six', 'wcwidth'],
+    install_requires=['wcwidth'],
 )
